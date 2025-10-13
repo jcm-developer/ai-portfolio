2c26b3c117d725edfbdcf60ff9b830b26cc5439a
jcm-developer <jaumecortesmonzon@gmail.com>
2025-10-13 17:45:36 +0200

    Configure Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jcm-developer.github.io',
+  base: '/my-portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
