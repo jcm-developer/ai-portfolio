2efc0f3ba6d33f07b3ede67168dbba70810bb886
jcm-developer <jaumecortesmonzon@gmail.com>
2025-10-13 18:29:01 +0200

    fix: add base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://jcm-developer.github.io",
   base: "/my-portfolio",
   trailingSlash: "always",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
